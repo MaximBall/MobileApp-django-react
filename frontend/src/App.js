5a81933d06f4f5d18fa694a814cbd2720a1fbdcb
MaximBall <maks1111bm@gmail.com>
2021-09-10 10:39:53 +0300

    add File NotesListPage.js for list notes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header'
+import NoteListPage from './pages/NotesListPage'
+
 
 function App() {
   return (
     <div className="App">
       <Header />
-      OKEY
+      <NoteListPage />
     </div>
   );
 }
